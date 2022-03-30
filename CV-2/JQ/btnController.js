07eb0bab16aa54d86c59af6c52b46390602d0fdc
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-03-30 13:17:23 +0500

    Update btnController.js

--- CV-2/JQ/btnController.js
+++ CV-2/JQ/btnController.js
@@ -4,12 +4,15 @@ $(function () {
   console.log("onload");
 
   // linked siblings
-  /*$("#home").click(function(){
-        $("#slide").addClass("d-none");
+  $("#home").click(function () {
+    // $(".slide").addClass("d-none");
+    console.log($(this));
+    // $(this).removeClass("d-none");
+    // x[this].classList.remove("d-none");
     console.log("Calling");
-
-    })*/
+  });
 });
+
 var slideIndex = 1;
 showDivs(slideIndex);
 // 0: button clicked -> 1: go to relaevant function (i.e. handleNext/handlePrev) -> 2: they will call plusDivs -> 3: it will call showDivs(n)
@@ -35,25 +38,9 @@ function showDivs(n) {
     slideIndex = x.length;
   }
   for (i = 0; i < x.length; i++) {
-  // const att = document.createAttribute("class");
-  // att.value = "d-none";
-
-    // x[i].style.display = "none";
-    // x[i].setAttributeNode(att); it replaces all attritubtes (overwrite)
     x[i].classList.add("d-none");
   }
-  // Create a class attribute:
-  // const att = document.createAttribute("class");
-  // Set the value of the class attribute:
-  // att.value = "d-block";
-
-  //const h1 = document.getElementsByTagName("H1")[0];
-  //h1.setAttributeNode(att);
-  // x[slideIndex - 1].style.display = "block";
-  // x[slideIndex - 1].setAttributeNode(att);
   x[slideIndex - 1].classList.remove("d-none");
-  // x[slideIndex - 1].classList.add("d-block");
-  
 }
 
 /*
