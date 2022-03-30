3c0ad2b64826d41951cda2ead6c44c31aa8dc67f
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-03-30 08:35:28 +0500

    Slide navigation (OK)
    Finally: dil ko qarar aya, pehli pehli bar aya
    
    Now, remove the bulk code

--- CV-2/JQ/btnController.js
+++ CV-2/JQ/btnController.js
@@ -1,14 +1,106 @@
-$(function(){
-    $("#nextSlide").click(handleNext)
-    $("#prevSlide").click(handlePrev)
-    handleNext();
-} )
+$(function () {
+  $("#nextSlide").click(handleNext);
+  $("#prevSlide").click(handlePrev);
+  console.log("onload");
 
+  // linked siblings
+  /*$("#home").click(function(){
+        $("#slide").addClass("d-none");
+    console.log("Calling");
+
+    })*/
+});
+var slideIndex = 1;
+showDivs(slideIndex);
+// 0: button clicked -> 1: go to relaevant function (i.e. handleNext/handlePrev) -> 2: they will call plusDivs -> 3: it will call showDivs(n)
+// 1
+function handleNext() {
+  plusDivs(+1);
+}
+function handlePrev() {
+  plusDivs(-1);
+}
+// 2
+function plusDivs(n) {
+  showDivs((slideIndex += n));
+}
+// 3
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {
+    slideIndex = 1;
+  }
+  if (n < 1) {
+    slideIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+  // const att = document.createAttribute("class");
+  // att.value = "d-none";
+
+    // x[i].style.display = "none";
+    // x[i].setAttributeNode(att); it replaces all attritubtes (overwrite)
+    x[i].classList.add("d-none");
+  }
+  // Create a class attribute:
+  // const att = document.createAttribute("class");
+  // Set the value of the class attribute:
+  // att.value = "d-block";
+
+  //const h1 = document.getElementsByTagName("H1")[0];
+  //h1.setAttributeNode(att);
+  // x[slideIndex - 1].style.display = "block";
+  // x[slideIndex - 1].setAttributeNode(att);
+  x[slideIndex - 1].classList.remove("d-none");
+  // x[slideIndex - 1].classList.add("d-block");
+  
+}
+
+/*
+function home(){
+    // $("#slide").addClass("d-none");
+    // $("#home").addClass("d-block");
+    // var s = $(".slide").siblings().css({"display" : "none"})
+    console.log("Calling");
+    //  s.addClass("d-none");
+    //  $(this).removeClass("d-none")
+    // $(this).addClass("rrrrrrrrrrrrrrrrrrrr");
+    // $(this).removeClass("rrrrrrrrrrrrrrrrrrrr");
+}
+
+
+function siblings()
+{
+
+}
 function handleNext()
 {
-    alert("Next button clicked")
+   // showDivs(+1);
+
+    console.log("Next button clicked");
+    // $("#slide").addClass("d-none")
+    // $(this).removeClass("d-none");
 }
-function handlePrev()
+function handlePrev(n)
 {
-    alert("Previous button clicked")
+  //  showDivs(-1);
+
+    console.log("Previous button clicked");
 }
+
+function showDivs(n) {
+    var i;
+    console.log("Val: "+n);
+    var x = document.getElementsByClassName("slide");
+    //$(".slide").addClass("d-none");
+    if (n > x.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = x.length} ;
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";
+      console.log("in loop");
+    }
+    x[slideIndex-1].style.display = "block";
+  }
+
+
+  console.log("before load");*/
