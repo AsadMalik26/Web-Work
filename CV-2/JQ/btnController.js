4ebf7466d03e26d2de2a414b2c2ded0fc9d2f8ff
AsadMalik26 <84629119+AsadMalik26@users.noreply.github.com>
2022-04-01 10:45:45 +0500

    Done
    Everything is done according to plan/idea
    
    Now it may need some enhancements like:
    1. Navigation button and link click synchronization
    2. responsiveness

--- CV-2/JQ/btnController.js
+++ CV-2/JQ/btnController.js
@@ -6,17 +6,62 @@ $(function () {
   // linked siblings
   $("#home").click(function () {
     // $(".slide").addClass("d-none");
-    console.log($(this));
+    // console.log($(this));
     // $(this).removeClass("d-none");
     // x[this].classList.remove("d-none");
     console.log("Calling");
   });
+  $(".contact").click(contactPage);
+
+  // individual link buttons
+  $(".home").click(homePage);
+  $(".about").click(aboutPage);
+  $(".edu").click(eduPage);
+  $(".skills").click(skillsPage);
+  $(".portfolio").click(portfolioPage);
+  $(".services").click(servicesPage);
 });
+function contactPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("contact");
+  c.classList.remove("d-none");
+}
+function homePage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("home");
+  c.classList.remove("d-none");
+}
+function aboutPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("about");
+  c.classList.remove("d-none");
+}
+function eduPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("education");
+  c.classList.remove("d-none");
+}
+function skillsPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("skills");
+  c.classList.remove("d-none");
+}
+function portfolioPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("portfolio");
+  c.classList.remove("d-none");
+}
+function servicesPage() {
+  $(".slide").addClass("d-none");
+  var c = document.getElementById("services");
+  c.classList.remove("d-none");
+}
+
 
 var slideIndex = 1;
 showDivs(slideIndex);
 // 0: button clicked -> 1: go to relaevant function (i.e. handleNext/handlePrev) -> 2: they will call plusDivs -> 3: it will call showDivs(n)
-// 1
+// 1  
 function handleNext() {
   plusDivs(+1);
 }
